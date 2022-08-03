fca07938c6efdf1525da97038dab508db4e753f1
Daniel-G07 <60954062+Daniel-G07@users.noreply.github.com>
2022-08-03 15:09:21 -0700

    added for loop

--- Test.java
+++ Test.java
@@ -2,5 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("push test ");
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
